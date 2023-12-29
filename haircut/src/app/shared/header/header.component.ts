c63e99cee381cd1257d3edcca870a9e30984ea1d
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-12-29 22:31:18 +0100

    added new section main contant

--- haircut/src/app/shared/header/header.component.ts
+++ haircut/src/app/shared/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  openMenu() {
+    let menuElement = document.getElementById('menu');
+    if (menuElement) {
+      menuElement.classList.toggle("change");
+    }
+  }
 }
