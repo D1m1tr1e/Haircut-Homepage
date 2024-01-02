406febb38a7bf929e786e2a0f37d2ed9e6a98c5e
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-01-02 22:56:55 +0100

    added slide in function for mobile menu

--- haircut/src/app/shared/header/header.component.ts
+++ haircut/src/app/shared/header/header.component.ts
@@ -9,10 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  mobileMenuIsOpen = false;
+
   openMenu() {
     let menuElement = document.getElementById('menu');
     if (menuElement) {
       menuElement.classList.toggle("change");
     }
+    this.openMobileMenu();
   }
-}
+
+  openMobileMenu() {
+    if (!this.mobileMenuIsOpen) {
+      document.getElementById('mobile-container')?.classList.remove('d-none');
+      this.mobileMenuIsOpen = true;
+    } else if (this.mobileMenuIsOpen) {
+      this.mobileMenuIsOpen = false;
+        document.getElementById('mobile-container')?.classList.add('d-none');
+      }
+    }
+
+  }
+
+
