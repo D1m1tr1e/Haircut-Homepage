8ebf2e8cb3d9dab29d63a7a7651cc2bf99954c08
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2024-01-03 13:18:16 +0100

    created some subcomponents

--- haircut/src/app/app.component.ts
+++ haircut/src/app/app.component.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { AgbComponent } from './agb/agb.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, MainContentComponent, FooterComponent ],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MainContentComponent,
+    FooterComponent, ImprintComponent, ContactComponent, AgbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- haircut/src/app/app.routes.ts
+++ haircut/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { ContactComponent } from './contact/contact.component';
+import { AgbComponent } from './agb/agb.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainContentComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'agb', component: AgbComponent }
+];
